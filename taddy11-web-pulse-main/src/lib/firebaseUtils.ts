adf67e6b8e0f50f8b8083349e2ac9e0ab5c84d21
CBKruthi <kruthicb651@gmail.com>
2025-11-07 17:40:03 +0530

    removed files

--- taddy11-web-pulse-main/src/lib/firebaseUtils.ts
+++
@@ -1,7 +0,0 @@
-// src/lib/firebaseUtils.ts
-import { getAuth } from "firebase/auth";
-
-export const getCurrentUserEmail = (): string | null => {
-  const auth = getAuth();
-  return auth.currentUser?.email || null;
-};
